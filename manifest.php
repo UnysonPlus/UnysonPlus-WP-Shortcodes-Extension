34094d6ce490533f25a544570008d3b29fcf7623
jonmlas <jonmlas@gmail.com>
2025-08-21 19:07:13 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,30 +1,42 @@
 <?php
 
-if (! defined('FW')) { die('Forbidden'); }
+if ( ! defined( 'FW' ) ) {
+    die( 'Forbidden' );
+}
 
-$manifest = array(
-	'name' => __('WordPress Shortcodes', 'fw'),
-	'description' => __(
-		'Lets you insert Unyson shortcodes inside any WordPress editor.',
-		'fw'
-	),
-	'uri' => 'http://manual.unyson.io/en/latest/extension/wp-shortcodes/index.html',
-	'github_repo' => 'https://github.com/ThemeFuse/Unyson-WP-Shortcodes-Extension',
-	'version' => '1.0.7',
-	'display' => true,
-	'standalone' => true,
-	
-	'author' => 'ThemeFuse',
-	'author_uri' => 'http://themefuse.com/',
+$manifest = array();
 
-	'requirements' => array(
-		'extensions' => array(
-			'shortcodes' => array(
-				'min_version' => '1.3.17'
-			)
-		)
-	)
+$manifest['name']        = __( 'WordPress Shortcodes', 'fw' );
+$manifest['slug']        = 'unysonplus-wp-shortcodes';
+$manifest['description'] = __( 
+    'Lets you insert Unyson shortcodes inside any WordPress editor.', 
+    'fw' 
 );
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-WP-Shortcodes-Extension';
+$manifest['version']     = '1.0.7';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
 
+// Requirements
+$manifest['requirements'] = array(
+    'extensions' => array(
+        'shortcodes' => array(
+            'min_version' => '1.3.17'
+        )
+    )
+);
+
+// Repo Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-WP-Shortcodes-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-WP-Shortcodes-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Extra Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
